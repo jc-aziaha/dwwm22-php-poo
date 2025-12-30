ce870627ee933b1d2bd90b22178ce08e25fd3174
Vega <aziaha.formations@gmail.com>
2025-12-30 16:36:46 +0100

    Résumé a

--- 4-abstract.php
+++ 4-abstract.php
@@ -1,36 +1,101 @@
 <?php
 
-    abstract class Animal {
+    abstract class Humain {
+        public ?string $raison = null;
 
-        public ?string $nom = null;
+        abstract function raisonner(): void;
 
-        abstract public function crier();
-
-        abstract public function chasser();
-
-        public function dormir(): void {
-            echo "L'animal dort";
+        public function manger(): void {
+            echo "Je mange<br>";
         }
     }
 
-    class Chien extends Animal {
-        public function crier() {
-            
+    class Vendeur extends Humain {
+
+        public array $qualites = ["commercial", "sociable"];
+
+        public function raisonner(): void {
+            echo "Le vendeur raisonne <br>";
         }
 
-        public function chasser() {
+        public function vendre(): void {
+            echo "Better Call Saul";
+        }
 
+        public function afficherQualites(): void {
+            foreach ($this->qualites as $qualite) {
+                echo $qualite . "<br>";
+            }
         }
     }
 
-    class Chat extends Animal {
-        public function crier() {
+    class Jardinier extends Humain {
+        public array $qualites = ["main verte", "être patient"];
 
+        public function raisonner(): void {
+            echo "Le jardinier raisonne <br>";
         }
 
-        public function chasser() {
-
+        public function planter(): void {
+            echo "Le jardinier plante";
         }
     }
 
-    new Animal;
+    class Chanteur extends Humain {
+        public function raisonner(): void {
+            echo "Le chanteur raisonne";
+        }
+
+        public function chanter(): void {
+            echo "Allumezzzzzzz le feu";
+        }
+    }
+
+    // $vendeur = new Vendeur();
+    // $vendeur->manger();
+    // $vendeur->afficherQualites();
+    // $vendeur->raisonner();
+
+
+    // $jardinier = new Jardinier();
+    // $jardinier->raisonner();
+
+    $chanteur = new Chanteur;
+    $chanteur->chanter();
+
+
+
+// -------------------------------------------------------------------------------
+
+    // abstract class Animal {
+
+    //     public ?string $nom = null;
+
+    //     abstract public function crier();
+
+    //     abstract public function chasser();
+
+    //     public function dormir(): void {
+    //         echo "L'animal dort";
+    //     }
+    // }
+
+    // class Chien extends Animal {
+    //     public function crier() {
+
+    //     }
+
+    //     public function chasser() {
+
+    //     }
+    // }
+
+    // class Chat extends Animal {
+    //     public function crier() {
+
+    //     }
+
+    //     public function chasser() {
+
+    //     }
+    // }
---
+++ 5-resume-a.php
@@ -0,0 +1,121 @@
+<?php
+
+    /**
+     * ***************************************************
+     * La poo en php
+     * 
+     * @todo Les concepts clés
+     *  1. La classe et les objets
+     *  2. Les propriétés et les méthodes de la classe et la notion d'encapsulation
+     *  3. Les accesseurs et les mutateurs et le mot clé $this
+     *  4. Le constructeur
+     * 
+     * ***************************************************
+     */
+
+
+    // ------------------------------------------------------------------------------------------------
+    // 1. La classe est une maquette sur laquelle l'on se base, afin de produire autant d'objets souhaités
+    // class Voiture {
+
+    // }
+
+    
+    // // 2. Les objets
+    // $voiture0 = new Voiture;
+    // $voiture1 = new Voiture;
+    // $voiture2 = new Voiture;
+    // $voiture3 = new Voiture;
+    // $voiture4 = new Voiture;
+    // $voiture5 = new Voiture;
+    // ------------------------------------------------------------------------------------------------
+
+    // 3. Les propriétés et les méthodes de la classe
+    // class Voiture {
+    //     public ?string $marque  = null;
+    //     public ?string $couleur = null;
+
+    //     public function demarrer(): void {
+    //         echo "La voiture démarre";
+    //     }
+
+    //     public function rouler(): void {
+    //         echo "La voiture roule";
+    //     }
+
+    //     public function freiner(): void {
+    //         echo "La voiture freine";
+    //     }
+    // }
+
+
+    // ------------------------------------------------------------------------------------------------
+    // 3. Les accesseurs (getters) et les mutateurs (setters)
+
+    // class Voiture {
+    //     private ?string $marque = null;
+    //     private ?string $couleur = null;
+
+    //     public function getMarque(): string {
+    //         return $this->marque;
+    //     }
+
+    //     public function setMarque(string $marque): void {
+    //         $this->marque = $marque;
+    //     }
+
+    //     public function getCouleur(): string {
+    //         return $this->couleur;
+    //     }
+
+    //     public function setCouleur(string $couleur): void {
+    //         $this->couleur = $couleur;
+    //     }
+    // }
+
+    // $voiture0 = new Voiture;
+    // $voiture0->setMarque("Fiat");
+    // echo $voiture0->getMarque();
+
+    // echo "<br>";
+
+    // $voiture1 = new Voiture;
+    // $voiture1->setMarque("Alfa Roméo");
+    // $voiture1->setCouleur("noire");
+    // echo "{$voiture1->getMarque()} de couleur {$voiture1->getCouleur()}";
+
+    // echo "<br>";
+
+
+    // ------------------------------------------------------------------------------------------------
+    // 4. Le constructeur (fonction magique)
+    class Voiture {
+        private ?string $marque = null;
+        private ?string $couleur = null;
+
+        public function __construct(string $marque, string $couleur)
+        {
+            $this->marque  = $marque;
+            $this->couleur = $couleur;
+        }
+
+        public function getMarque(): string {
+            return $this->marque;
+        }
+
+        public function setMarque(string $marque): void {
+            $this->marque = $marque;
+        }
+
+        public function getCouleur(): string {
+            return $this->couleur;
+        }
+
+        public function setCouleur(string $couleur): void {
+            $this->couleur = $couleur;
+        }
+    }
+
+    $voiture0 = new Voiture("Ferrari", "rouge");
+    echo $voiture0->getMarque();
+
