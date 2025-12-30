08fe9805b6fddbbe8eb939da1b30681faa610e20
Vega <aziaha.formations@gmail.com>
2025-12-30 10:53:42 +0100

    Explication du cours d'hier dans le nouveau fichier index.php

--- index.php
+++ index.php
@@ -1,143 +1,86 @@
 <?php
 
-
-    class User {
-
-        /**
-         * Ceci est l'identifiant de chaque utilisateur.
-         *
-         * @var null|integer
-         */
-        private ?int $id = null;
-
+    class Voiture {
 
         /**
-         * Ceci représente le prénom de chaque utilisateur.
-         *
-         * @var null|string
+         * Ceci représente la marque de chaque voiture
+         * 
+         * @var string|null
          */
-        private ?string $firstName = null;
-
+        private ?string $marque = null;
 
         /**
-         * Ceci représente le nom de chaque utilisateur.
-         *
-         * @var null|string
+         * Ceci représente la couleur de chaque voiture
+         * 
+         * @var string|null
          */
-        private ?string $lastName = null;
-
+        private ?string $couleur = null;
 
         /**
-         * Ceci représente l'email de chaque utilisateur.
-         *
-         * @var string
+         * Ceci représente le model de chaque voiture
+         * 
+         * @var string|null
          */
-        private ?string $email = null;
+        private ?string $model = null;
 
+        public function __construct(string $marque, string $couleur, string $model) {
+            $this->marque  = $marque;
+            $this->couleur = $couleur;
+            $this->model   = $model;
+        }
 
-        /**
-         * Ceci représente le mot de passe de chaque utilisateur.
-         *
-         * @var string
-         */
-        private ?string $password = null;
+        public function rouler(): void {
+            echo "Vroum";
+        }
 
+        public function freiner(): void {
+            echo "Frrrrrrrrr";
+        }
 
-        public function __construct(string $firstName, string $lastName, string $email, string $password)
-        {
-            $this->firstName = $firstName;
-            $this->lastName  = $lastName;
-            $this->email     = $email;
-            $this->password  = $password;
+        // -----------------------  Les accesseurs (getters) -------------------------
+        public function getMarque(): string {
+            return $this->marque;
+        }
+
+        public function getCouleur(): string {
+            return $this->couleur;
+        }
+
+        public function getModel(): string {
+            return $this->model;
         }
 
 
-        // -----------------------Accesseurs (getters)------------------------
-        public function getId(): int {
-            return $this->id;
+
+        // -----------------------  Les mutateurs (setters) -------------------------
+        public function setMarque(string $marque): void {
+            $this->marque = $marque;
         }
 
-        public function getFirstName(): string {
-            return $this->firstName;
+        public function setCouleur(string $couleur): void {
+            $this->couleur = $couleur;
         }
 
-        public function getLastName(): string {
-            return $this->lastName;
+        public function setModel(string $model): void {
+            $this->model = $model;
         }
 
-        public function getEmail(): string {
-            return $this->email;
+        public function getDisplay(): string {
+
+            return "La voiture de marque: {$this->marque} dont le model est {$this->model} est de couleur {$this->couleur}";
         }
 
-        public function getPassword(): string {
-            return $this->password;
-        }
-
-        // -----------------------Mutateurs (setters)------------------------
-        public function setFirstName(string $firstName): void {
-            $this->firstName = $firstName;
-        }
-
-        public function setLastName(string $lastName): void {
-            $this->lastName = $lastName;
-        }
-
-        public function setEmail(string $email): void {
-            $this->email = $email;
-        }
-
-        public function setPassword(string $password): void {
-            $this->password = $password;
-        }
     }
 
-    $user1 = new User("Picou", "Balthazar", "picsou@gmail.com", "pipi1234A*");
-
-    $user1->setFirstName("Picsou");
-    // $user1->setLastName("Balthazar");
-    // $user1->setEmail("picsou@gmail.com");
-    // $user1->setPassword("pipi1234A*");
-
-    echo "Les données de l'utilisateur: {$user1->getFirstName()} {$user1->getLastName()} <br>";
-
-    // echo "<br>";
-
-    // $user2 = new User;
-    // $user2->setFirstName("Donald");
-    // $user2->setLastName("Duck");
-    // $user2->setEmail("donald@gmail.com");
-    // $user2->setPassword("dodo1234A*");
-    // echo "Les données de l'utilisateur: {$user2->getFirstName()} {$user2->getLastName()} <br>";
-
+    $v1 = new Voiture("Mercedes", "argentée", "190-D");
+    $v1->setCouleur("noire");
+    echo $v1->getDisplay();
     
-    // class Robot {
-    //     public string $head = "une tête ronde";
-    //     protected string $body = "un tronc adapté";
-    //     private int $footer = 3;
-
-    //     public function getHead(): string {
-    //         return $this->head;
-    //     }
-
-    //     public function getBody(): string {
-    //         return $this->body;
-    //     }
-
-    //     public function getFooter(): int {
-    //         return $this->footer;
-    //     }
-    // }
-
-    // $robot0 = new Robot;
-    // echo "Le robot 0 a {$robot0->head}. <br>";
-    // $robot0->head = "Une tête carrée";
-    // echo "Le robot 0 a  {$robot0->head} maintenant. <br>";
+    echo "<br>";
     
-    // echo "<br>";
-    
-    // $robot1 = new Robot;
-    // echo "Le robot 1 a {$robot1->head}. <br>";
-    // echo "Le robot 1 a {$robot1->getBody()}. <br>";
-    // echo "Le robot 1 a {$robot1->getFooter()} pieds.<br>";
+    $v2 = new Voiture("Fiat", "bleue", "bravo");
+    echo $v2->getDisplay();
 
-?>
+
+
+
